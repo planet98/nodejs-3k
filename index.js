4669457c3186fe47975ace36526d616f8075c024
tony <planet@hongxin.eu.org>
2023-10-10 10:07:00 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // 引入 createVLESSServer 函数
 const { createVLESSServer } = require("@3kmfi6hp/nodejs-proxy");
 // 定义端口和 UUID
-const port = 3001;
+const port = 3000;
 const uuid = "d342d11e-d424-4583-b36e-524ab1f0afa4";
 
 // 调用函数启动 VLESS 服务器
